c2dd390886e3c4905f489aea09d601c88387c734
Ajit <kshatriyakavisingh@gmail.com>
2024-10-06 11:07:43 +0530

    Adding typing animation using Typed.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 import heroImage from '../assets/img/hero/hero-home.jpg';
 
 const Hero = () => {
+
+  const highlightText = useRef(null);
+  const typed = useRef(null);
+
+  useEffect(() => {
+    // Options for Typed.js
+    const options = {
+      strings: ['solutions', 'strategies', 'innovations'], // Add your desired phrases here
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: true,
+    };
+
+    // Initialize Typed.js on the highlightText ref
+    typed.current = new Typed(highlightText.current, options);
+
+    return () => {
+      // Destroy Typed.js instance during cleanup to prevent memory leaks
+      typed.current.destroy();
+    };
+  }, []);
+
+
+
   return (
 
 <div className="hero-content">
@@ -11,7 +36,7 @@ const Hero = () => {
   <h1>
     We provide easy 
     <br/>
-    <span className="highlight">solutions </span>
+    <span className="highlight" ref={highlightText}>solutions </span>
     for startups <br /> 
     <span className="bold-text">at affordable rates.</span>
   </h1>
